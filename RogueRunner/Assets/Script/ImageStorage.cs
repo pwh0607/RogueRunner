ad2e4e5dfc63d6cd053a4d5fa0e33c216eafae30
pwh0607 <120497748+pwh0607@users.noreply.github.com>
2024-10-08 22:56:29 +0900

    스크립트 추가 커밋(2)
    image asset 인스턴스화 용 저장 스크립트

---
+++ RogueRunner/Assets/Script/ImageStorage.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageStorage : MonoBehaviour
+{
+    public List<GameObject> images;
+
+    void Start()
+    {
+        images = new List<GameObject>();
+    }
+
+
+}
